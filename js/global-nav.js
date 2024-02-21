869b0e0073ba53175e1984d921b5e1007252f9fa
Romain Edelmann <romain.edelmann@gmail.com>
2024-02-21 20:21:41 +0100

    Add secondary navigation when screen permits

--- js/global-nav.js
+++ js/global-nav.js
@@ -17,9 +17,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // Select all headers in the sections of the page
     // and add an anchor link to each one.
-    let selector = "section > h1";
+    let selector = "section > header > h2, section > h1";
     for (let i = 2; i <= 6; i++) {
         selector += ", section > h" + i;
+        selector += ", .cols-2 > div > h" + i;
     }
     const headers = document.querySelectorAll(selector);
     const usedIds = {};
@@ -48,6 +49,94 @@ document.addEventListener("DOMContentLoaded", function(event) {
             });
             // Change the URL to include the hash.
             window.history.pushState(null, null, "#" + id);
+        });
+    }
+
+    // Setup the secondary navigation.
+    const secNav = document.getElementById('sec-nav');
+    const section = document.querySelector('#main-content > section');
+    const headings = section.querySelectorAll('h2, h3, h4');
+
+    let headings_count = 0;
+    for (let i = 0; i < headings.length; i++) {
+        const heading = headings[i];
+        if (!heading.id) {
+            continue;
+        }
+        headings_count++;
+        const link = document.createElement('a');
+        
+        link.href = '#' + heading.id;
+        link.textContent = heading.textContent.replace(/¶$/, '');
+
+        // Set the class based on the heading level.
+        if (heading.tagName === 'H2') {
+            link.classList.add('primary');
+        } else if (heading.tagName === 'H3') {
+            link.classList.add('secondary');
+        }
+        else if (heading.tagName === 'H4') {
+            link.classList.add('tertiary');
+        }
+        
+
+        // Gently scroll to the header when the link is clicked.
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            document.getElementById(heading.id).scrollIntoView({
+                behavior: 'smooth',
+            });
+            window.history.pushState(null, null, '#' + heading.id);
+        });
+
+        secNav.appendChild(link);
+    }
+    if (headings_count < 2) {
+        secNav.style.display = 'none';
+    }
+    else {
+
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                const id = entry.target.id;
+                const is_active = entry.intersectionRatio >= 0.5;
+                const link = secNav.querySelector(`a[href="#${id}"]`);
+                link.classList.toggle('active', is_active);
+
+                if (is_active) {
+                    link.classList.remove('before-active');
+                }
+                else {
+                    const top = entry.boundingClientRect.top;
+                    if (top < 0) {
+                        link.classList.add('before-active');
+                    }
+                    else {
+                        link.classList.remove('before-active');
+                    }
+                }
+            });
+
+            let has_active = secNav.querySelector("a.active") !== null;
+
+            if (has_active) {
+                const links = secNav.querySelectorAll("a");
+                let is_before = true;
+                for (let i = 0; i < links.length; i++) {
+                    const link = links.item(i);
+                    if (link.classList.contains("active")) {
+                        is_before = false;
+                    }
+                    link.classList.toggle("before-active", is_before);
+                }
+            }
+        }, { threshold: [0.5] });
+
+        headings.forEach(heading => {
+            if (!heading.id) {
+                return;
+            }
+            observer.observe(heading);
         });
     }
 
